b80cb940724329c951bf9beccb8ce893b2c1f020
Tommy Ku <zoo1929@outlook.com>
2019-03-30 19:41:03 +0800

    FEAT: server CORS

--- server/index.php
+++ server/index.php
@@ -27,6 +27,19 @@ $container['database'] = function () {
     ]);
 };
 
+// CORS
+$app->options('/{routes:.+}', function ($request, $response, $args) {
+    return $response;
+});
+
+$app->add(function ($req, $res, $next) {
+    $response = $next($req, $res);
+    return $response
+        ->withHeader('Access-Control-Allow-Origin', '*')
+        ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+        ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+});
+
 /**
  * GET getApiKey
  * Summary: Create and get a new API key
